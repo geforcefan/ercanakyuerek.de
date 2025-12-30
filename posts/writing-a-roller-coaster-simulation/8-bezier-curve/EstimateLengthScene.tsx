f71d433538cb6a5dc71e70c0f91c52d335ca8992
geforcefan <geforcefan@users.noreply.github.com>
2025-12-30 19:52:11 +0000

    deploy: d40157c1f9b4172ec02a834b2d237340db98b0da

---
+++ posts/writing-a-roller-coaster-simulation/8-bezier-curve/EstimateLengthScene.tsx
@@ -0,0 +1,59 @@
+import React, { useMemo, useState } from 'react';
+import { Line } from '@react-three/drei';
+import { useControls } from 'leva';
+import { Vector3 } from 'three';
+
+import {
+  deCasteljau,
+} from '../../../../maths/bezier';
+import {
+  uniformSampleMap,
+} from '../../../../helper/uniform-sample';
+import { useColors } from '../../../../hooks/useColors';
+
+import { BezierCurve } from '../../../../components/BezierCurve';
+import { DragControlPoints } from '../../../../components/DragControlPoints';
+import { OrthographicScene } from '../../../../scenes/OrthographicScene';
+
+export const EstimateLengthScene = () => {
+  const colors = useColors();
+
+  const [state] = useControls(() => ({
+    numberOfNodes: {
+      min: 2,
+      max: 16,
+      step: 1,
+      value: 8,
+    },
+  }));
+
+  const [points, setPoints] = useState([
+    new Vector3(-4.5, -6, 0),
+    new Vector3(-4.5, 2.8, 0),
+    new Vector3(4.5, 2.8, 0),
+    new Vector3(4.5, -6, 0),
+  ]);
+
+  const positions = useMemo(
+    () =>
+      uniformSampleMap(0, state.numberOfNodes, 1, (at, t) => deCasteljau(points, t)),
+    [state.numberOfNodes, points],
+  );
+
+  return (
+    <OrthographicScene>
+      <Line points={positions}/>
+      <Line
+        points={points}
+        color={colors.highlight}
+        segments={true}
+      />
+      <DragControlPoints
+        axisLock="z"
+        points={points}
+        setPoints={setPoints}
+      />
+      <BezierCurve points={points} color={colors.highlight} />
+    </OrthographicScene>
+  );
+};
