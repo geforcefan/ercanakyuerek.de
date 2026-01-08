d8004ef6cef9b3292f599da8f6ab99e7fccbda0c
Ercan Akyürek <ercan.akyuerek@justtrade.com>
2026-01-08 19:18:27 +0100

    test

--- src/scenes/OrthographicScene.tsx
+++ src/scenes/OrthographicScene.tsx
@@ -8,7 +8,8 @@ import { useColors } from '../hooks/useColors';
 import { SplineEditorCameraControls } from '../components/camera/SplineEditorCameraControls';
 
 export const OrthographicScene = ({
-  cameraControlsActive = true, ...props
+  cameraControlsActive = true,
+  ...props
 }: {
   children?: ReactNode;
   cameraControlsActive?: boolean;
