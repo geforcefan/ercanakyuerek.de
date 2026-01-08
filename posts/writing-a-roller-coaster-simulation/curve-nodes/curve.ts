3a8965d963a859dbaa71051ce387ed5bacf208ee
geforcefan <geforcefan@users.noreply.github.com>
2026-01-08 23:34:53 +0000

    deploy: 3e0d3be1234ab897f285f988e8a222d62a52f1cb

---
+++ posts/writing-a-roller-coaster-simulation/curve-nodes/curve.ts
@@ -0,0 +1,47 @@
+import last from 'lodash/last';
+import { Matrix4, Vector3 } from 'three';
+
+import { empty } from '../../../../maths/curve';
+
+export const fromPoints = (points: Vector3[]) => {
+  const curve = empty();
+  if (points.length < 2) return curve;
+
+  let arcLength = 0;
+
+  for (let i = 0; i < points.length - 1; i++) {
+    const left = points[i];
+    const right = points[i + 1];
+    const prevNode = curve.nodes[curve.nodes.length - 1];
+
+    if (prevNode)
+      curve.nodes.push({
+        matrix: prevNode.matrix.clone().setPosition(left),
+        arcLength,
+        segmentIndex: 0,
+      });
+
+    curve.nodes.push({
+      matrix: new Matrix4()
+        .lookAt(right, left, new Vector3(0, 1, 0))
+        .setPosition(left),
+      arcLength,
+      segmentIndex: 0,
+    });
+
+    arcLength += left.distanceTo(right);
+  }
+
+  const lastNode = last(curve.nodes)!;
+  const lastPoint = last(points)!;
+
+  curve.nodes.push({
+    matrix: lastNode.matrix.clone().setPosition(lastPoint),
+    arcLength,
+    segmentIndex: 0,
+  });
+
+  curve.segmentOffsets.push(arcLength);
+
+  return curve;
+};
---
+++ posts/writing-a-roller-coaster-simulation/linear-track/physics.ts
@@ -0,0 +1,45 @@
+import { Vector3 } from 'three';
+
+import { SimulationState } from '../../../../helper/physics';
+
+export const evaluateMotion = (
+  state: SimulationState,
+  forwardDirection: Vector3,
+  gravity: number,
+  deltaTime: number,
+): SimulationState => {
+  const acceleration = forwardDirection.dot(
+    new Vector3(0, -gravity, 0),
+  );
+  const velocity = state.velocity + acceleration * deltaTime;
+  const distanceTraveled =
+    state.distanceTraveled + velocity * deltaTime;
+
+  return { velocity, distanceTraveled, acceleration };
+};
+
+export const evaluateMotionWithFriction = (
+  state: SimulationState,
+  forwardDirection: Vector3,
+  friction: number,
+  airResistance: number,
+  gravity: number,
+  deltaTime: number,
+): SimulationState => {
+  const velocityDirection = state.velocity < 0 ? -1 : 1;
+
+  let energyLoss = airResistance * state.velocity * state.velocity;
+  energyLoss += friction * gravity;
+  energyLoss *= velocityDirection;
+
+  let acceleration = forwardDirection.dot(
+    new Vector3(0, -gravity, 0),
+  );
+  acceleration -= energyLoss;
+
+  const velocity = state.velocity + acceleration * deltaTime;
+  const distanceTraveled =
+    state.distanceTraveled + velocity * deltaTime;
+
+  return { velocity, distanceTraveled, acceleration };
+};
