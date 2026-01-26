44edad1256dcc5a0ef7644090e390e0dc51784ed
Ercan Akyürek <ercan.akyuerek@justtrade.com>
2026-01-27 00:12:19 +0100

    little optimized

--- src/components/scenes/EditorScene.tsx
+++ src/components/scenes/EditorScene.tsx
@@ -1,6 +1,5 @@
 import React, { ReactNode } from 'react';
 import { CanvasProps } from '@react-three/fiber/dist/declarations/src/web/Canvas';
-import { Vector3 } from 'three';
 
 import {
   CameraView,
@@ -11,18 +10,20 @@ import { PerspectiveEditor } from '../editor/PerspectiveEditor';
 import { DefaultCanvas } from './DefaultCanvas';
 
 export const EditorScene = ({
-  boundary = new Vector3(500, 500, 500),
   children,
 }: {
   children?: ReactNode;
-  boundary?: Vector3;
 } & CanvasProps) => {
   return (
     <CameraViewManager>
       <DefaultCanvas>
         {['front', 'back', 'top', 'bottom', 'left', 'right'].map(
           (view) => (
-            <CameraView name={view} makeDefault={view === 'front'}>
+            <CameraView
+              key={view}
+              name={view}
+              makeDefault={view === 'front'}
+            >
               <OrthographicEditor view={view as any} />
             </CameraView>
           ),
