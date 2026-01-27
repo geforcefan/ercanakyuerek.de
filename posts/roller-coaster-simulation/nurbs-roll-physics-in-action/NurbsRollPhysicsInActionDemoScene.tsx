d52c7de86eda8664671d0bdb2f320bb0c45e944c
geforcefan <geforcefan@users.noreply.github.com>
2026-01-27 00:43:38 +0000

    deploy: 9ec57fe6af282157cf3148fa4c88595b0998ddd8

--- posts/roller-coaster-simulation/nurbs-roll-physics-in-action/NurbsRollPhysicsInActionDemoScene.tsx
+++ posts/roller-coaster-simulation/nurbs-roll-physics-in-action/NurbsRollPhysicsInActionDemoScene.tsx
@@ -7,8 +7,7 @@ import { fromURL } from '../../../../helper/nl2park/nl2park';
 import { curveFromCustomTrack } from '../../../../helper/nolimits';
 
 import { CurveTrackMesh } from '../../../../components/curve/CurveTrackMesh';
-import { Ground } from '../../../../components/Ground';
-import { PerspectiveScene } from '../../../../components/scenes/PerspectiveScene';
+import { AtmosphericPerspectiveScene } from '../../../../components/scenes/AtmosphericPerspectiveScene';
 import { TrainWithPhysics } from '../../../../components/TrainWithPhysics';
 
 import Park from './Hybris.nl2park';
@@ -24,9 +23,16 @@ const exampleTrackCurve = toLocalTransformed(
 export const NurbsRollPhysicsInActionDemoScene = () => {
   return (
     <>
-      <PerspectiveScene>
+      <AtmosphericPerspectiveScene>
         <Stats />
-        <Ground position={new Vector3(0, -7, 0)} />
+
+        <group receiveShadow={true} position={[0, -7, 0]}>
+          <mesh receiveShadow={true} rotation-x={-Math.PI / 2}>
+            <planeGeometry args={[1000, 1000]} />
+            <meshStandardMaterial color={'dark-green'} />
+          </mesh>
+        </group>
+
         <CurveTrackMesh curve={exampleTrackCurve} />
         <TrainWithPhysics
           curve={exampleTrackCurve}
@@ -38,7 +44,7 @@ export const NurbsRollPhysicsInActionDemoScene = () => {
           }}
           activateCamera={true}
         />
-      </PerspectiveScene>
+      </AtmosphericPerspectiveScene>
     </>
   );
 };
