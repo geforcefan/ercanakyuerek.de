68bac52d998d25dd9a7369c0814bd5c08cde07f5
geforcefan <geforcefan@users.noreply.github.com>
2026-01-29 20:28:55 +0000

    deploy: 07f8ac59fdd1c33320e7f9111637c64c78ed595f

---
+++ posts/writing-a-roller-coaster-simulation/b-spline-curve/BSplineClampedExampleScene.tsx
@@ -0,0 +1,46 @@
+import React, { useMemo, useState } from 'react';
+import { Line } from '@react-three/drei';
+import { Vector3, Vector4 } from 'three';
+
+import { useColors } from '../../../../hooks/useColors';
+
+import { CurveLine } from '../../../../components/curve/CurveLine';
+import { DragControlPoints } from '../../../../components/curve/DragControlPoints';
+import { OrthographicScene } from '../../../../components/scenes/OrthographicScene';
+
+import { fromPoints as bSplineTrackFromPoints } from '../../../../coaster/b-spline-track';
+
+export const BSplineClampedExampleScene = () => {
+  const colors = useColors();
+
+  const [points, setPoints] = useState([
+    new Vector3(2, 0, 0),
+    new Vector3(0, 2, 0),
+    new Vector3(0, 6, 0),
+    new Vector3(6, 6, 0),
+    new Vector3(6, 2, 0),
+    new Vector3(4, 0, 0),
+  ]);
+
+  const curve = useMemo(
+    () =>
+      bSplineTrackFromPoints(
+        points.map((p) => new Vector4(p.x, p.y, p.z)),
+      ),
+    [points],
+  );
+
+  return (
+    <OrthographicScene>
+      <group position={[-3, -2, 0]}>
+        <Line points={points} color={colors.highlight} />
+        <DragControlPoints
+          axisLock="z"
+          points={points}
+          setPoints={setPoints}
+        />
+        <CurveLine curve={curve} />
+      </group>
+    </OrthographicScene>
+  );
+};
